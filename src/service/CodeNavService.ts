ceb7e06d4a5a5941b8b944f3b86ca6869e0150a6
viqueen <hrehioui@atlassian.com>
2021-02-11 19:57:46 +1100

    noissue: welcome CodeNavService

---
+++ src/service/CodeNavService.ts
@@ -0,0 +1,17 @@
+import { SimpleTaskExecutor } from './SimpleTaskExecutor';
+
+export class CodeNavService implements Service {
+    readonly store!: Store;
+    readonly executor!: TaskExecutor;
+
+    constructor(store: Store, maxConcurrent: number = 5) {
+        this.store = store;
+        this.executor = new SimpleTaskExecutor(maxConcurrent);
+    }
+
+    _execute(command: Command, filter: ItemFilter): void {
+        this.store.list(filter).then((items) => {
+            items.map(command.process).forEach(this.executor.submit);
+        });
+    }
+}
