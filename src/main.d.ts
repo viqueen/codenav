2cd8e9850cb0d9d55ac03254fbf01dcc17511032
viqueen <hrehioui@atlassian.com>
2021-02-11 20:00:25 +1100

    noissue: consolidate types into one main file

--- src/main.d.ts
+++ src/main.d.ts
@@ -1,3 +1,5 @@
+// DATA
+
 interface Item {
     readonly ID: string;
     readonly connection: string;
@@ -23,4 +25,26 @@ interface Store {
     add(item: Item): Promise<Item>;
     remove(filter: ItemFilter): Promise<Array<Item>>;
     list(filter: ItemFilter): Promise<Array<Item>>;
+}
+
+// SERVICE
+
+interface Task {
+    run(): Promise<any>;
+}
+
+interface TaskExecutor {
+    readonly queue: Array<Task>;
+    readonly maxConcurrent: number;
+    submit(task: Task): void;
+}
+
+interface Command {
+    process(item: Item): Task;
+}
+
+interface Service {
+    readonly store: Store;
+    readonly executor: TaskExecutor;
+    _execute(command: Command, filter: ItemFilter): void;
 }
