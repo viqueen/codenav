24bfb7fa35ff88a67730ae3e0c66972268a05669
viqueen <hrehioui@atlassian.com>
2021-03-04 20:19:50 +1100

    noissue: skip clone for existing git directories

--- src/command/CloneCommand.ts
+++ src/command/CloneCommand.ts
@@ -1,6 +1,8 @@
 import { Item, ItemCommand, ItemLocation } from '../main';
 import { Task } from 'task-pool-executor';
 import simpleGit from 'simple-git';
+import path from 'path';
+import { existsSync } from 'fs';
 
 export class CloneCommand implements ItemCommand {
     readonly location!: ItemLocation;
@@ -11,6 +13,12 @@ export class CloneCommand implements ItemCommand {
 
     make(item: Item): Task {
         const target = this.location.resolve(item);
+        if (existsSync(path.resolve(target, '.git'))) {
+            return () => {
+                console.log(`already checked out : ${target}`);
+                return Promise.resolve();
+            };
+        }
         return () =>
             simpleGit()
                 .clone(item.connection, target)
